82f1364c089534f5031c2f11cc828939f81e4e6a
Avani Anand <iamavanianand@gmail.com>
2023-08-23 17:31:47 +0530

    Question no.2 solved

--- solution.js
+++ solution.js
@@ -1,5 +1,29 @@
+// QNO.1 SOLUTION
+
 let totalvalue=2000;
 let discountpercentage=20;
 let finalprice=totalvalue-(totalvalue*discountpercentage/100);
 
 console.log("the final price after discount is: Rs."+finalprice );
+
+// QNO.2 SOLUTION
+
+
+
+
+
+
+
+
+
+
+
+
+// QNO.3 SOLUTION
+// QNO.4 SOLUTION
+// QNO.5 SOLUTION
+// QNO.6 SOLUTION
+// QNO.7 SOLUTION
+// QNO.8 SOLUTION
+// QNO.9 SOLUTION
+// QNO.10 SOLUTION
