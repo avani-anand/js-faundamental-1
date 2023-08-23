4bb0eb11b85d23ef937fc72e7120725c2a60bc64
Avani Anand <iamavanianand@gmail.com>
2023-08-23 17:16:51 +0530

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,5 @@
+let totalvalue=2000;
+let discountpercentage=20;
+let finalprice=totalvalue-(totalvalue*discountpercentage/100);
+
+console.log("the final price after discount is: Rs."+finalprice );
